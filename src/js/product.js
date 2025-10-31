3e980971a0d76dfe901d7f57d5e8fd7f57e6edaf
smondragondev <smondragnsilva@byupathway.edu>
2025-10-31 14:10:51 -0500

    Fixing the cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,15 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get products from local storage
+
+  let productsInCart = getLocalStorage("so-cart");
+  if (!productsInCart) productsInCart = [];
+  productsInCart.push(product);
+  setLocalStorage("so-cart", productsInCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
