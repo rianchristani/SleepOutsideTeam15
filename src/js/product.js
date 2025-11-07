6e9b54d7e3ce21ccdb14bac2d1c95c630482cbf8
smondragondev <smondragnsilva@byupathway.edu>
2025-11-06 21:40:41 -0500

    add dynamic product detail

--- src/js/product.js
+++ src/js/product.js
@@ -1,23 +1,9 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
+const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  // Get products from local storage
-
-  let productsInCart = getLocalStorage("so-cart");
-  if (!productsInCart) productsInCart = [];
-  productsInCart.push(product);
-  setLocalStorage("so-cart", productsInCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,12 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+
+// get a url params
+export function getParam(param){
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const paramValue = urlParams.get(param);
+  return paramValue;
+}
