55f7ab935c334652e5514325282787a3fce27da0
christiannataren <cnatarnlaguna@byupathway.edu>
2025-11-15 18:40:33 -0500

    Revert "searching"
    This reverts commit b419460bb2482e06f1ef598ce5b8a09593176d2e.

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,4 +1,5 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
+
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -14,8 +15,6 @@ export default class ProductData {
   } */
 
   async getData(category) {
-    let url = `${baseURL}products/search/${category} `
-    console.log(url);
     const response = await fetch(`${baseURL}products/search/${category} `);
     const data = await convertToJson(response);
     return data.Result;
